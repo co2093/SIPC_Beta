5ef157ce7340b57ce9be14c9403a9eef287d4b3f
BrallanASR13002 <sr13002@ues.edu.sv>
2024-02-06 21:12:20 -0600

    configuracion inicial

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-
+Route::get('/investiadores',function(){
+    return view('investigadores');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
